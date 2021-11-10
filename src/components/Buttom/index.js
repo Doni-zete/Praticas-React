d14ab63509c06a837af283c32e961be294b4ae69
Doni-zete <donizete112@gmail.com>
2021-11-09 21:11:10 -0300

    first commit

---
+++ src/components/Buttom/index.js
@@ -0,0 +1,11 @@
+const Button =({children, className, onClick}) => {
+  return(
+    <button type="button"
+          class={className}
+          onClick={onClick}>
+          
+          {children}</button>
+  )
+}
+
+export default Button;
